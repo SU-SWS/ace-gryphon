951c66588b8c7eef28b684437aa3d6f42eb16865
Mike Decker <pookmish@gmail.com>
2020-02-26 17:23:47 -0800

    Initial GRYPHON

---
+++ blt/src/Blt/Plugin/Commands/GryphonHooksCommands.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Example\Blt\Plugin\Commands;
+
+use Acquia\Blt\Robo\BltTasks;
+use Acquia\Blt\Robo\Common\EnvironmentDetector;
+use Robo\Contract\VerbosityThresholdInterface;
+
+class GryphonHooksCommands extends BltTasks {
+
+  /**
+   * @hook pre-command source:build:simplesamlphp-config
+   */
+  public function preSamlConfigCopy() {
+    $task = $this->taskFilesystemStack()
+      ->stopOnFail()
+      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE);
+    $repo_root = $this->getConfigValue('repo.root');
+    $copy_map = [
+      $repo_root . '/simplesamlphp/config/default.local.config.php' => $repo_root . '/simplesamlphp/config/local.config.php',
+      $repo_root . '/simplesamlphp/config/default.local.authsources.php' => $repo_root . '/simplesamlphp/config/local.authsources.php',
+    ];
+    foreach ($copy_map as $from => $to) {
+      if (!file_exists($to)) {
+        $task->copy($from, $to);
+      }
+    }
+    $task->run();
+    foreach ($copy_map as $to) {
+      $this->getConfig()->expandFileProperties($to);
+    }
+  }
+
+}
