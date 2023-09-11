0eab6d64bedb951f6ef40cbe7853e0c22562dcc9
Mike Decker <pookmish@gmail.com>
2023-09-11 14:06:33 -0700

    update samlauth settings

--- docroot/sites/settings/default.local.settings.php
+++ docroot/sites/settings/default.local.settings.php
@@ -16,3 +16,28 @@ $config['stanford_ssp.settings'] = [
   'workgroup_api_cert' => DRUPAL_ROOT . '/../keys/workgroup_api.cert',
   'workgroup_api_key' => DRUPAL_ROOT . '/../keys/workgroup_api.key',
 ];
+
+/**
+ * SAMLAuth configuration
+ */
+$config['samlauth.authentication']['sp_x509_certificate'] = 'file:' . DRUPAL_ROOT . '/../keys/saml.crt';
+$config['samlauth.authentication']['sp_private_key'] = 'file:' . DRUPAL_ROOT . '/../keys/saml.pem';
+$config['samlauth.authentication']['idp_certs'] = [
+  'file:' . DRUPAL_ROOT . '/../keys/signing.crt',
+];
+$config['stanford_samlauth.settings']['role_mapping']['workgroup_api'] = [
+  'cert' => DRUPAL_ROOT . '/../keys/workgroup_api.cert',
+  'key' => DRUPAL_ROOT . '/../keys/workgroup_api.key',
+];
+
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+// Saml login doesn't work on gitpod or tugboat, don't set config values.
+if (getenv('GITPOD_WORKSPACE_URL') || getenv('TUGBOAT_REPO')) {
+  unset($config['samlauth.authentication']);
+
+  $config['stanford_samlauth.settings'] = [
+    'hide_local_login' => FALSE,
+    'local_login_fieldset_open' => true,
+  ];
+}
